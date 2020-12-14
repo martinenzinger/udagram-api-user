b9a51b2e75006fb91cb540e475c24ac94b1d45fd
Martin Enzinger <martin.enzinger@gmail.com>
2020-12-14 13:41:19 +0100

    fix typo

--- src/controllers/v0/users/routes/user.router.ts
+++ src/controllers/v0/users/routes/user.router.ts
@@ -10,9 +10,9 @@ router.use('/auth', AuthRouter);
 router.get('/');
 
 router.get('/:id', async (req: Request, res: Response) => {
+  const {id} = req.params;
   let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(new Date().toLocaleString() + `: ${ip} GET /users/${id} - Get user info`);
-  const {id} = req.params;
   const item = await User.findByPk(id);
   res.send(item);
 });
