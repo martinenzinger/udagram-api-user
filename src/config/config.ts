d8674c5d14ba153351839fe4f6367599f5bbc655
Martin Enzinger <martin.enzinger@gmail.com>
2020-12-14 12:51:01 +0100

    update service config

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,9 @@ export const config = {
   'database': process.env.POSTGRES_DB,
   'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
+  'aws_region': process.env.UDAGRAM_AWS_REGION,
+  'aws_profile': process.env.UDAGRAM_AWS_PROFILE,
+  'aws_media_bucket': process.env.UDAGRAM_AWS_BUCKET,
   'url': process.env.SERVER_URL,
   'jwt': {
     'secret': process.env.UDAGRAM_JWT_SECRET,
